363401c26ad95e273c126b6eb487c2c98d38ad1d
shubham singh <43708880+shubh5900@users.noreply.github.com>
2023-11-11 04:10:14 +0000

    application setting and urls templates create

--- DashBoard/app/views.py
+++ DashBoard/app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def Login(request):
+    return render(request,"Login.html")
+def Home(request):
+    return render(request,"Home.html")
